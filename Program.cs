6893ec7b1cf8a78e0b77f51a85056652134a5540
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-12-12 11:22:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hmm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foodType ovo = new foodType("Jagoda",4, 6, 4);
+            Console.WriteLine(ovo.ToString());
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
---
+++ foodType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hmm
+{
+    internal class foodType
+    {
+        string name;
+        int carbs, fat;
+        int protein;
+        static int count=0;
+
+        public foodType(string name, int carbs, int fat, int protein)
+        {
+           this.name = name;
+           this.carbs = carbs;
+           this.fat = fat;
+           this.protein = protein;
+           count++;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Carbs { get => carbs; set => carbs = value; }
+        public int Fat { get => fat; set => fat = value; }
+        public int Protein { get => protein; set => protein = value; }
+        public static int Count { get => count; set => count = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Ime proizvoda: " + this.name + "\n"
+                + "Proteini: " + this.protein + "%\n"
+                + "Carbs: " + this.carbs + "%\n" 
+                + "Fat: " + this.fat + "%\n"
+                + "Counter: " + count + "\n";
+            return ispis;
+        }
+    }
+}
